d733e66fe434ce55405c05ddcba253fdc12fa853
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-25 13:38:40 +0900

    :memo: ignore tag追加

--- envs/providers.tf
+++ envs/providers.tf
@@ -1,8 +1,17 @@
 terraform {
   required_providers {
     oci = {
-      source = "oracle/oci"
+      source  = "oracle/oci"
       version = "~> 8.0"
     }
   }
+}
+
+provider "oci" {
+  region = var.region
+  ignore_defined_tags = [
+    "Oracle-Tags.CreatedBy",
+    "Oracle-Tags.CreatedOn",
+    "Common.System"
+  ]
 }
